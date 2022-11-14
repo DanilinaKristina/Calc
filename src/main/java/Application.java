be5877fc6c94dd67c186ba202a365ad2217eebe4
DanilinaKristina <kris.chachkova@gmail.com>
2022-11-14 13:16:57 +0300

    finis

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,2 +1,42 @@
-package PACKAGE_NAME;public class Application {
+public class Application {
+    public static void main(String[] args) {
+        double a = Double.parseDouble(args[0]);
+        double b = Double.parseDouble(args[1]);
+        if(args[2].equals("+")){
+            System.out.println(Sum(a,b));
+        }
+        if(args[2].equals("*")){
+            System.out.println(Multiplier(a,b));
+        }
+        if(args[2].equals("/")){
+            System.out.println(Div(a,b));
+        }
+        if(args[2].equals("-")){
+            System.out.println(Minus(a,b));
+        }
+        if(args[2].equals("%")){
+            System.out.println(Ostatok(a,b));
+        }
+        if(args[2].equals("^")){
+            System.out.println(Stepen(a,b));
+        }
+    }
+    public static double Sum (double a, double b ){
+        return a + b;
+    }
+    public static double Div (double a, double b ){
+        return a/b;
+    }
+    public static double Minus (double a, double b ){
+        return a-b;
+    }
+    public static double Multiplier (double a, double b ){
+        return a*b;
+    }
+    public static double Ostatok (double a, double b){
+        return a%b;
+    }
+    public static double Stepen (double a, double b){
+        return Math.pow(a,b);
+    }
 }
